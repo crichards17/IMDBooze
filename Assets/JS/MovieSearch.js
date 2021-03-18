b68166dc08fdad979be8217d4f0a65f521c44335
Cameron Richards <cameron.richards@salesforce.com>
2021-03-17 23:00:25 -0700

    functional draft, updated MovieClass

--- Assets/JS/MovieSearch.js
+++ Assets/JS/MovieSearch.js
@@ -1,22 +1,27 @@
-var searchButtonEl = document.querySelector('#search-button');
-var searchBoxEl = document.querySelector('#search-box');
-var resultsListEl = document.querySelector('#search-results');
+const searchButtonEl = $('#search-button');
+const searchBoxEl = $('#search-box');
+const resultsListEl = $('#search-results');
+const step2El = $('#step-2');
+const movieTitleEl = $('#movie-title');
+const posterEl = $('#poster-container');
+const moviePlotEl = $('#movie-plot');
+const movieRatingEl = $('#imdb-rating');
 
 const apiURL = 'http://www.omdbapi.com/?apikey=c8c161fc';
 
 function searchMedia() {
-    let searchURL = apiURL + `&s=${searchBoxEl.value}`;
+    let searchURL = apiURL + `&s=${searchBoxEl.val()}`;
     fetch(searchURL)
   .then(response => response.json())
   .then(data => printResults(data));
 }
 
 function printResults(data) {
-    resultsListEl.innerHTML = "";
+    resultsListEl.html('');
+    step2El.removeClass('hidden');
     let results = data.Search;
     for (let i = 0; i < results.length; i++) {
-        let newLi = document.createElement('li');
-        newLi.innerText = results[i].Title;
+        let newLi = $(`<div class="result-card">${results[i].Title}</div>`);
         resultsListEl.append(newLi);
     }
 }
@@ -30,6 +35,7 @@ function getDetails(title) {
 }
 
 function passMovie(data) {
+    printMovie(data);
     let media = new Movie(data.Title);
     media.year = data.Year;
     media.genre = data.Genre;
@@ -44,15 +50,21 @@ function passMovie(data) {
     media.website = data.Website;
 }
 
-// Displaying data to Movie card:
-/*
-moviePosterEl.inner
+function printMovie(data) {
+    movieTitleEl.text(data.Title);
+    posterEl.html(`<img src=${data.Poster} style="height:100%"></img>`);
+    moviePlotEl.text(`Plot: ${data.Plot}`);
+    movieRatingEl.text(`IMDB Rating: ${data.Ratings[0].Value}`);
+}
 
-*/
-
-searchButtonEl.addEventListener('click',searchMedia);
-searchBoxEl.addEventListener('keyup', function(event) {
+searchButtonEl.on('click',searchMedia);
+searchBoxEl.on('keyup', function(event) {
     if (event.keyCode === 13) {
-        searchMovie();
+        searchMedia();
     }
+});
+$(document).on('click', '.result-card', function(event) {
+    let title = $(event.target).text();
+    getDetails(title);
+    // passDetails(title);
 });
